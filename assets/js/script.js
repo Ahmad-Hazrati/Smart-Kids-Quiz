e9a0e3f0b13c85a4121a901717f75e7ccee0f0ee
Ahmad-Hazrati <hazrati.ahmad@gmail.com>
2023-07-26 23:04:24 +0200

    Add coding for start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
+// Variables declaration
+const startBtn = document.querySelector('.start-btn');
+const popupInfo = document.querySelector('.popup-info');
 
-
-
+//Start button function 
+startBtn.onclick = () => {
+    popupInfo.classList.add('active');
+    main.classList.add('active');
+}
 
 
 
