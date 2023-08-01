9e3499897f7d6c116761a8ba8cf30ed742f4f0c5
Ahmad-Hazrati <hazrati.ahmad@gmail.com>
2023-08-01 17:03:09 +0200

    Add coding to change span text based on user selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 // Import questions from questions.js
-import { kg4_7_Questions, kg8_10_Questions, kg10_12_Questions } from "./questions.js";
-
+import {
+    kg4_7_Questions,
+    kg8_10_Questions,
+    kg10_12_Questions,
+} from "./questions.js";
 
 //Variables declaration
 //Audio
@@ -10,108 +13,119 @@ const correctSound = new Audio("./assets/audio/correct_answer_sound.mp3");
 const incorrectSound = new Audio("./assets/audio/incorrect_answer_sound.mp3");
 const quizOver = new Audio("./assets/audio/quiz_over.mp3");
 
-const soundOn = document.getElementById('sound-on');
-const soundOff = document.getElementById('sound-off');
+const soundOn = document.getElementById("sound-on");
+const soundOff = document.getElementById("sound-off");
 
 //buttons and containers
-const startBtn = document.querySelector('.start-btn');
-const popupInfo = document.querySelector('.popup-info');
-const exitBtn = document.querySelector('.exit-btn');
-const main = document.querySelector('.main');
-const continueBtn = document.querySelector('.continue-btn');
-const quizSection = document.querySelector('.quiz-section');
-const quizBox = document.querySelector('.quiz-box');
-const resultBox = document.querySelector('.result-box');
-const tryAgainBtn = document.querySelector('.tryAgain-btn');
-const goHomeBtn = document.querySelector('.goHome-btn');
+const startBtn = document.querySelector(".start-btn");
+const popupInfo = document.querySelector(".popup-info");
+const exitBtn = document.querySelector(".exit-btn");
+const main = document.querySelector(".main");
+const continueBtn = document.querySelector(".continue-btn");
+const quizSection = document.querySelector(".quiz-section");
+const quizBox = document.querySelector(".quiz-box");
+const resultBox = document.querySelector(".result-box");
+const tryAgainBtn = document.querySelector(".tryAgain-btn");
+const goHomeBtn = document.querySelector(".goHome-btn");
 const ageSelection = document.querySelectorAll('.age-selection input[type="radio"]');
+const quizCategoryElement = document.querySelector(".quiz-category");
 let questions;
 
 // taken from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffle(array) {
-    let currentIndex = array.length, randomIndex;
+    let currentIndex = array.length,
+        randomIndex;
 
     // While there remain elements to shuffle.
     while (currentIndex != 0) {
-
         // Pick a remaining element.
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex--;
 
         // And swap it with the current element.
         [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]];
+            array[randomIndex],
+            array[currentIndex],
+        ];
     }
     return array;
 }
 
-
 // Wait for DOM to finish loading before running the quiz
-// Get the audio elements and add event listners to them
+// Get the audio elements and add event listeners to them
 document.addEventListener("DOMContentLoaded", function () {
-    backgroundMusic.play();
+    // backgroundMusic.play();
     /* changes the audio to muted */
-    soundOn.addEventListener('click', function () {
+    soundOn.addEventListener("click", function () {
         backgroundMusic.muted = true;
         incorrectSound.muted = true;
         correctSound.muted = true;
         clickSound.muted = true;
         quizOver.muted = true;
-        soundOff.classList.add('active');
-        soundOn.classList.remove('active');
+        soundOff.classList.add("active");
+        soundOn.classList.remove("active");
     });
 
     /* changes the audio to unmuted */
-    soundOff.addEventListener('click', function () {
+    soundOff.addEventListener("click", function () {
         backgroundMusic.muted = false;
         incorrectSound.muted = false;
         correctSound.muted = false;
         clickSound.muted = false;
         quizOver.muted = false;
-        soundOff.classList.remove('active');
-        soundOn.classList.add('active');
+        soundOff.classList.remove("active");
+        soundOn.classList.add("active");
     });
 
     // start button function
     startBtn.onclick = () => {
         clickSound.play();
-        popupInfo.classList.add('active');
-        main.classList.add('active');
+        popupInfo.classList.add("active");
+        main.classList.add("active");
     };
 
     // exit button function
     exitBtn.onclick = () => {
         clickSound.play();
-        popupInfo.classList.remove('active');
-        main.classList.remove('active');
+        popupInfo.classList.remove("active");
+        main.classList.remove("active");
     };
 
     /**
      * age selection function gets 10 random questions from questions.js
      * based on the user's selection
      */
-    ageSelection.forEach(selector => {
-        selector.addEventListener('change', () => {
-            continueBtn.removeAttribute('disabled');
-            if (selector.id === 'kg4-7') {
+    ageSelection.forEach((selector) => {
+        selector.addEventListener("change", () => {
+            continueBtn.removeAttribute("disabled");
+            if (selector.id === "kg4-7") {
                 questions = kg4_7_Questions;
-            } else if (selector.id === 'kg8-10') {
+            } else if (selector.id === "kg8-10") {
                 questions = kg8_10_Questions;
             } else {
                 questions = kg10_12_Questions;
             }
+            changeText();
             questions = shuffle(questions).slice(0, 10);
-        }
-        );
+        });
     });
+
+    /**
+     * this function changes the text of span in quiz section 
+     * based on the user user quiz category selection
+     */
+    function changeText() {
+        let category = document.querySelector('input[name="grade"]:checked').value;
+      quizCategoryElement.textContent = category;
+    }
 
     // continue button function
     continueBtn.onclick = () => {
         clickSound.play();
-        quizSection.classList.add('active');
-        popupInfo.classList.remove('active');
-        main.classList.remove('active');
-        quizBox.classList.add('active');
+        quizSection.classList.add("active");
+        popupInfo.classList.remove("active");
+        main.classList.remove("active");
+        quizBox.classList.add("active");
 
         showQuestions(0);
         questionCounter(1);
@@ -121,9 +135,9 @@ document.addEventListener("DOMContentLoaded", function () {
     // try again button function
     tryAgainBtn.onclick = () => {
         clickSound.play();
-        quizBox.classList.add('active');
-        nextBtn.classList.remove('active');
-        resultBox.classList.remove('active');
+        quizBox.classList.add("active");
+        nextBtn.classList.remove("active");
+        resultBox.classList.remove("active");
 
         questionCount = 0;
         questionNumb = 1;
@@ -137,9 +151,9 @@ document.addEventListener("DOMContentLoaded", function () {
     // go to home button function
     goHomeBtn.onclick = () => {
         clickSound.play();
-        quizSection.classList.remove('active');
-        nextBtn.classList.remove('active');
-        resultBox.classList.remove('active');
+        quizSection.classList.remove("active");
+        nextBtn.classList.remove("active");
+        resultBox.classList.remove("active");
 
         questionCount = 0;
         questionNumb = 1;
@@ -151,15 +165,13 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 });
 
-
 let questionCount = 0;
 let questionNumb = 1;
 let userScore = 0;
 
+const nextBtn = document.querySelector(".next-btn");
 
-const nextBtn = document.querySelector('.next-btn');
-
-// next button function 
+// next button function
 nextBtn.onclick = () => {
     clickSound.play();
     if (questionCount < questions.length - 1) {
@@ -168,37 +180,36 @@ nextBtn.onclick = () => {
 
         questionNumb++;
         questionCounter(questionNumb);
-        nextBtn.classList.remove('active');
+        nextBtn.classList.remove("active");
     } else {
         quizOver.play();
         showResultBox();
     }
 };
 
+const optionList = document.querySelector(".option-list");
 
-const optionList = document.querySelector('.option-list');
-
-/** 
+/**
  * This function shows the questions */
 function showQuestions(index) {
-    const questionText = document.querySelector('.question-text');
+    const questionText = document.querySelector(".question-text");
     questionText.textContent = `${questions[index].question}`;
 
     let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
-    <div class="option"><span>${questions[index].options[1]}</span></div>
-    <div class="option"><span>${questions[index].options[2]}</span></div>
-    <div class="option"><span>${questions[index].options[3]}</span></div>`;
+      <div class="option"><span>${questions[index].options[1]}</span></div>
+      <div class="option"><span>${questions[index].options[2]}</span></div>
+      <div class="option"><span>${questions[index].options[3]}</span></div>`;
 
     optionList.innerHTML = optionTag;
 
-    const option = document.querySelectorAll('.option');
+    const option = document.querySelectorAll(".option");
     for (let i = 0; i < option.length; i++) {
-        option[i].addEventListener('click', optionSelected);
+        option[i].addEventListener("click", optionSelected);
     }
 }
 
-/** 
- * This function checks if the user has selected the correct answer, 
+/**
+ * This function checks if the user has selected the correct answer,
  * otherwise shows the correct answer
  */
 function optionSelected(answer) {
@@ -208,63 +219,61 @@ function optionSelected(answer) {
 
     if (userAnswer === correctAnswer) {
         correctSound.play();
-        answer.target.classList.add('correct');
+        answer.target.classList.add("correct");
         userScore++;
         headerScore();
     } else {
         incorrectSound.play();
-        answer.target.classList.add('incorrect');
+        answer.target.classList.add("incorrect");
 
         // if user selected answer is incorrect answer, selects auto correct answer
         for (let i = 0; i < allOptions; i++) {
             if (optionList.children[i].textContent == correctAnswer) {
-                optionList.children[i].setAttribute('class', 'option correct');
-
+                optionList.children[i].setAttribute("class", "option correct");
             }
         }
     }
 
     // if user has selected an option, disable all other options
     for (let i = 0; i < allOptions; i++) {
-        optionList.children[i].classList.add('disabled');
+        optionList.children[i].classList.add("disabled");
     }
-    nextBtn.classList.add('active');
+    nextBtn.classList.add("active");
 }
 
 function questionCounter(index) {
-    const questionTotal = document.querySelector('.question-total');
+    const questionTotal = document.querySelector(".question-total");
     questionTotal.textContent = `${index} of ${questions.length} questions`;
 }
 
 function headerScore() {
-    const headerScoreText = document.querySelector('.header-score');
+    const headerScoreText = document.querySelector(".header-score");
     headerScoreText.textContent = `Score: ${userScore} / ${questions.length}`;
 }
 
-
-/** 
+/**
  * This function shows the result of the score */
 function showResultBox() {
-    quizBox.classList.remove('active');
-    resultBox.classList.add('active');
+    quizBox.classList.remove("active");
+    resultBox.classList.add("active");
 
-    const scoreText = document.querySelector('.score-text');
+    const scoreText = document.querySelector(".score-text");
     scoreText.textContent = `Your Score ${userScore} out of ${questions.length}`;
 
-    const circularProgress = document.querySelector('.circular-progress');
-    const progressValue = document.querySelector('.progress-value');
+    const circularProgress = document.querySelector(".circular-progress");
+    const progressValue = document.querySelector(".progress-value");
 
     let progressStartValue = 0;
     let progressEndValue = (userScore / questions.length) * 100;
-    let speed = 10;
+    let speed = 20;
 
     let progress = setInterval(() => {
-        progressStartValue++;
-
         progressValue.textContent = `${progressStartValue}%`;
-        circularProgress.style.background = `conic-gradient(#0086D2 ${progressStartValue * 3.6}deg, rgba(255, 255, 255, .1) 0deg)`;
-        if (progressStartValue == progressEndValue) {
+        circularProgress.style.background = `conic-gradient(#0086D2 ${progressStartValue * 3.6
+            }deg, rgba(255, 255, 255, .1) 0deg)`;
+        if (progressStartValue === progressEndValue) {
             clearInterval(progress);
         }
+        progressStartValue++;
     }, speed);
 }
